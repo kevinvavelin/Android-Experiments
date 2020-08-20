6f57880b5d0827e832a052df712993c5e34346f3
Kevin Vavelin <kvavelin@geek-inc.fr>
2020-08-20 15:06:13 +0200

    Initial Commit

---
+++ app/src/main/java/com/kevinvavelin/android_experiments/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kevinvavelin.android_experiments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
